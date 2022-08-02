9a36bfce75a24a626497af17447737a8d3265281
Julie DEE <juliedee@mbp-de-julie.lan>
2022-08-02 14:18:34 +0200

    resolve stripe errors

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -3,16 +3,20 @@ const router = express.Router();
 const stripe = require("stripe")(process.env.STRIPE);
 
 router.post("/payment", async (req, res) => {
-  const stripeToken = req.body.stripeToken;
-  const response = await stripe.charges.create({
-    amount: req.body.price,
-    currency: "eur",
-    description: req.body.name,
-    source: stripeToken,
-  });
-  console.log(response.status);
+  try {
+    const stripeToken = req.body.stripeToken;
+    const response = await stripe.charges.create({
+      source: stripeToken,
+      amount: req.body.price * 100,
+      currency: "eur",
+      description: req.body.name,
+    });
+    console.log(response.status);
 
-  res.json(response);
+    res.json(response);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
